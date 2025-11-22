ff5cea3474c6aa372599596e5f7c9457549ba053
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-22 13:12:27 +0500

    tictactoe base stage 1

--- src/main/java/com/example/apij_project/controllers/TicTacToeController.java
+++ src/main/java/com/example/apij_project/controllers/TicTacToeController.java
@@ -1,4 +1,36 @@
 package com.example.apij_project.controllers;
 
+
+import com.example.apij_project.AppNavigator;
+import com.example.apij_project.logic.ScoreManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class TicTacToeController {
-}
+
+    @FXML private Label scoreLbl;
+    @FXML private Label statusLbl;
+    @FXML private Button nextButton;
+
+    @FXML private Button btn00, btn01, btn02;
+    @FXML private Button btn10, btn11, btn12;
+    @FXML private Button btn20, btn21, btn22;
+
+    private Button[][] grid;
+    private boolean finished = false;
+
+    @FXML
+    public void initialize() {
+        scoreLbl.setText("Score: " + ScoreManager.getScore());
+
+        grid = new Button[][] {
+                { btn00, btn01, btn02 },
+                { btn10, btn11, btn12 },
+                { btn20, btn21, btn22 }
+        };
+    }
