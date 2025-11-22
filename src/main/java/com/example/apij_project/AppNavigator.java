b037135bd8323de631e9dfff772416f7fb99e86f
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-22 19:10:13 +0500

    tictactoe style

--- src/main/java/com/example/apij_project/AppNavigator.java
+++ src/main/java/com/example/apij_project/AppNavigator.java
@@ -16,8 +16,11 @@ public class AppNavigator {
             FXMLLoader loader = new FXMLLoader(AppNavigator.class.getResource("/com/example/apij_project/views/" + fxmlFile));
             Scene scene = new Scene(loader.load());
             scene.getStylesheets().add(
-                    AppNavigator.class.getResource("/com/example/apij_project/styles/main.css").toExternalForm()
+                    AppNavigator.class.getResource("/com/example/apij_project/styles/main.css")
+                            .toExternalForm()
             );
+            scene.getStylesheets().add(AppNavigator.class
+                    .getResource("/com/example/apij_project/styles/tictactoe.css").toExternalForm());
             mainStage.setScene(scene);
             mainStage.show();
         } catch (Exception e) {
