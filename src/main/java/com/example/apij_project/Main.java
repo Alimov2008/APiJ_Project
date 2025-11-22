f580b171b1131f9455aec068454ff9503cde8613
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-22 12:44:19 +0500

    main window caller (main)

--- src/main/java/com/example/apij_project/Main.java
+++ src/main/java/com/example/apij_project/Main.java
@@ -1,5 +1,16 @@
 package com.example.apij_project;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        AppNavigator.setStage(stage);
+        AppNavigator.switchScene("start-view.fxml");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
